f8cf0b67dbbbc308615cab30a32fad64369ea5e6
Luis <luribesdf@gmail.com>
2018-04-02 03:33:50 -0600

    last commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pepper Jack Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tequila Burger', true);
